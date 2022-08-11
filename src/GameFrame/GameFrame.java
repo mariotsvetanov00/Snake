63398479ccb55f1a867ce2434a82096876a441c0
mariotsvetanov00 <mariotsvetanov00@gmail.com>
2022-08-11 15:02:13 +0300

    commit

---
+++ src/GameFrame/GameFrame.java
@@ -0,0 +1,20 @@
+package GameFrame;
+
+import GamePanel.GamePanel;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+    public GameFrame() {
+
+        GamePanel panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
---
+++ src/SnakeGame.java
@@ -0,0 +1,8 @@
+import GameFrame.GameFrame;
+
+public class SnakeGame {
+    public static void main(String[] args) {
+
+        new GameFrame();
+    }
+}
